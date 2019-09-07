1ac55b56c56aaf04615d33b7b7b3d49cf878a1f4
Pawel Szczesny <pszczesny89@gmail.com>
2019-09-07 12:21:44 +0200

    dodanie encji kierowcy

---
+++ src/main/java/pl/sda/rentcar/entity/Drivers.java
@@ -0,0 +1,25 @@
+package pl.sda.rentcar.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Drivers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+}
