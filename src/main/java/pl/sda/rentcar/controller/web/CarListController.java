6875e8997b81b9e7d33bb9933943c1b72026b00f
Karol Adamczyk <adamczyk.karol7475@gmail.com>
2019-09-08 13:38:42 +0200

    dodanie html ze stroną główną, html do dodawania samochodów i metod w kontrolerze do dodawania samochodów

---
+++ src/main/java/pl/sda/rentcar/controller/web/CarListController.java
@@ -0,0 +1,43 @@
+package pl.sda.rentcar.controller.web;
+
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import pl.sda.rentcar.dtos.CarDTO;
+import pl.sda.rentcar.service.CarService;
+
+@Controller
+@RequestMapping("/cars")
+@RequiredArgsConstructor
+public class CarListController {
+    //private final static Logger LOGGER = LoggerFactory.getLogger(CarListController.class);
+    private final CarService carService;
+
+    @RequestMapping("/list")
+    ModelAndView getCars() {
+        ModelAndView modelAndView = new ModelAndView("/carlist.html");
+        modelAndView.addObject("cars", carService.getAll());
+        return modelAndView;
+    }
+
+    @GetMapping("/create")
+    ModelAndView createCarView() {
+        ModelAndView modelAndView = new ModelAndView("/createcar.html");
+        modelAndView.addObject("car", new CarDTO());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    String createCar(CarDTO dto) {
+        carService.add(dto);
+        return "redirect:/";
+    }
+
+}
---
+++ src/main/java/pl/sda/rentcar/controller/web/MainViewController.java
@@ -0,0 +1,17 @@
+package pl.sda.rentcar.controller.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
+
+@Controller
+public class MainViewController {
+    @RequestMapping
+    ModelAndView mainView() {
+        ModelAndView mav = new ModelAndView("index.html");
+        return mav;
+    }
+}
